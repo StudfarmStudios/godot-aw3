1ab56be7761079338605fca5449821dd908153fc
Stuart Carnie <stuart.carnie@gmail.com>
2026-07-23 13:03:47 +1000

    metal: fix various issues with Metal rendering; improve presentation
    Co-authored-by: Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
    (cherry picked from commit 01795c253776cd7183eed7137a409a3126c067b1)

--- drivers/metal/rendering_device_driver_metal3.h
+++ drivers/metal/rendering_device_driver_metal3.h
@@ -78,14 +78,19 @@ class API_AVAILABLE(macos(11.0), ios(14.0), tvos(14.0)) RenderingDeviceDriverMet
 
 	Vector<MDCommandBuffer *> command_buffers;
 
-	Error _create_device() override;
+	// GPU timestamps require barrier synchronization, so the level fences order the
+	// timestamp encoders against the work they bracket.
+	bool timestamp_queries_supported = false;
+
 	Error _execute_and_present_barriers(CommandQueueID p_cmd_queue, VectorView<SemaphoreID> p_wait_semaphores, VectorView<CommandBufferID> p_cmd_buffers, VectorView<SemaphoreID> p_cmd_semaphores, FenceID p_cmd_fence, VectorView<SwapChainID> p_swap_chains);
 	Error _execute_and_present(CommandQueueID p_cmd_queue, VectorView<SemaphoreID> p_wait_semaphores, VectorView<CommandBufferID> p_cmd_buffers, VectorView<SemaphoreID> p_cmd_semaphores, FenceID p_cmd_fence, VectorView<SwapChainID> p_swap_chains);
 
 protected:
+	Error _create_device() override;
+	void _resolve_sync_mode() override;
 	MTL::CommandQueue *get_command_queue() const override { return device_queue.get(); }
-	void add_residency_set_to_main_queue(MTL::ResidencySet *p_set) override;
-	void remove_residency_set_to_main_queue(MTL::ResidencySet *p_set) override;
+	void add_residency_set_to_main_queue(MTL::ResidencySet *p_set) override API_AVAILABLE(macos(15.0), ios(18.0), tvos(18.0), visionos(2.0));
+	void remove_residency_set_to_main_queue(MTL::ResidencySet *p_set) override API_AVAILABLE(macos(15.0), ios(18.0), tvos(18.0), visionos(2.0));
 
 public:
 	Error initialize(uint32_t p_device_index, uint32_t p_frame_count) override;
@@ -106,6 +111,14 @@ public:
 	void command_pool_free(CommandPoolID p_cmd_pool) override;
 
 	CommandBufferID command_buffer_create(CommandPoolID p_cmd_pool) override;
+
+#pragma mark - Timestamp
+
+	QueryPoolID timestamp_query_pool_create(uint32_t p_query_count) override;
+	void timestamp_query_pool_free(QueryPoolID p_pool_id) override;
+	void timestamp_query_pool_get_results(QueryPoolID p_pool_id, uint32_t p_query_count, uint64_t *r_results) override;
+	uint64_t timestamp_query_result_to_time(uint64_t p_result) override;
+	void command_timestamp_write(CommandBufferID p_cmd_buffer, QueryPoolID p_pool_id, uint32_t p_index) override;
 
 #pragma mark - Miscellaneous
 
